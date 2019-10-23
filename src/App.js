3188da76a3fe2b6611f41ca880bce97b79d50f66
Joe <mendietaczfls@gmail.com>
2019-10-23 23:41:42 +1100

    Able to signin with google account

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import './App.css';
 import Home from './screens/home'
 import Amplify from 'aws-amplify'
 import aws_config from "./aws-exports"
+import { withAuthenticator, Authenticator } from 'aws-amplify-react'
 
 Amplify.configure(aws_config)
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <div>
+        <Authenticator usernameAttributes='email'/>
+        <Home />
+      </div>
+      
     );
   }
 }
 
+// export default withAuthenticator(App, true);
 export default App;
