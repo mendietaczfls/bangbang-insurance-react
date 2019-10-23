1287330b4073b8a46539c1268b58de02713b4910
Joe <mendietaczfls@gmail.com>
2019-10-23 22:02:34 +1100

    Amplify with GraphQL Basic

---
+++ src/screens/home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import AppNavBar from '../components/appNavBar'
+import ListItems from '../components/listItems'
+
+class Home extends Component {
+  render() {
+    return (
+        <div>
+            <AppNavBar />
+            <ListItems />
+        </div>    
+    );
+  }
+}
+
+export default Home;
